8af4d57e863ec53c6dc2f323a67b8a9d3da1ee1b
Kate-Lav <aklavender@gmail.com>
2025-07-26 22:56:25 +0100

    Updated Typewriter plugin settings

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ function generateCocktail(event) {
   event.preventDefault();
 
   new Typewriter("#recipe", {
-    strings: ["Aperol Spritz: ice, 100ml Aperol, 150ml Prosecco, soda"],
+    strings: "Aperol Spritz: ice, 100ml Aperol, 150ml Prosecco, soda",
     autoStart: true,
-    delay: 1,
+    delay: 50,
     cursor: "",
   });
 }
